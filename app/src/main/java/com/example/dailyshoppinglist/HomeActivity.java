30936438f1bd0b25952090ed2fd8ff970d709ed4
Shivam Tyagi <shivamtyagi837@gmail.com>
2020-01-03 08:20:02 +0530

    Update HomeActivity.java

--- app/src/main/java/com/example/dailyshoppinglist/HomeActivity.java
+++ app/src/main/java/com/example/dailyshoppinglist/HomeActivity.java
@@ -125,7 +125,6 @@ public class HomeActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-
             }
         });
 
